aa0c01027d809c615c67b445ba92d48088abe2a6
Deng Yu <jardy@vip.qq.com>
2020-01-05 22:01:29 +0800

    添加项目文件。

---
+++ StructsAndEnums/Program.cs
@@ -0,0 +1,39 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace StructsAndEnums
+{
+    class Program
+    {
+        static void doWork()
+        {
+            //Month first = Month.November;
+            //first += 3;
+            //Console.WriteLine(first);
+
+            Date defaultDate = new Date();
+            Console.WriteLine(defaultDate);
+            Date weddingAnniversary = new Date(2019, Month.November, 25);
+            Console.WriteLine(weddingAnniversary);
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
